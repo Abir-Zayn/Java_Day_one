df2b7ad9542497a5b46f259652459fe16cbecca1
Abir-Zayn <62856303+Abir-Zayn@users.noreply.github.com>
2021-09-27 02:02:17 +0600

    Add files via upload

---
+++ javainstalled.java
@@ -0,0 +1,6 @@
+public class javainstalled {
+    public static void main(String[] args) {
+        System.out.println("Java Version: "+System.getProperty("java.version") );
+        System.out.println("Java home: "+System.getProperty("java.home"));
+    }
+}
